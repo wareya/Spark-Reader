67d6510e5f0b9dffdfea9117c92c1379de0ce3ed
wareya <wareya@gmail.com>
2017-04-27 22:48:09 -0400

    Add some failing tests

---
+++ heavy/tests/Deconjugation.java
@@ -0,0 +1,49 @@
+import language.dictionary.Dictionary;
+import language.segmenter.Segmenter;
+import language.splitter.FoundWord;
+import language.splitter.WordSplitter;
+import main.Main;
+import options.Known;
+import options.Options;
+import options.PrefDef;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test for the heuristics used to prevent the word splitter from inventing words
+ */
+public class Deconjugation
+{
+    @Test
+    public void testDeconjugation() throws IOException
+    {
+        Segmenter.extended = true;
+        Segmenter.instance = new HeavySegmenter();
+        Main.options = new Options();
+
+        Main.known = new Known(Main.options.getFile("knownWordsPath"));
+        Main.prefDef = new PrefDef(Main.options.getFile("preferredDefsPath"));
+
+        Main.options.setOption("splitterMode", "full");
+        Main.options.setOption("deconMode", "recursive");
+        Main.options.setOption("kuromojiSupportLevel", "heuristics");
+
+        Dictionary dict = new Dictionary(new File("../dictionaries"));
+        WordSplitter splitter = new WordSplitter(dict);
+
+        //ArrayList<Piece> pieces = Segmenter.instance.Segment();
+        List<FoundWord> words;
+
+        words = splitter.split("それじゃ最近してたことって何？",  new HashSet<>());
+        for(FoundWord word : words)
+        {
+            assertEquals(word.getText().equals("た"), false); // false split
+        }
+    }
+}
